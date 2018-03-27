29391da022579db27b27aea3c54d463380cc90bc
Sam Kissee <samkissee@gmail.com>
2018-03-26 19:07:41 -0600

    update workshops

--- assets/workshops.js
+++ assets/workshops.js
@@ -2,27 +2,35 @@ import { Images } from './../App/Themes'
 
 export const workshopList = [
   {
-    'name': 'Water Yogo',
-    'bio': "Post-ironic pour-over venmo mlkshk drinking vinegar, palo santo PBR&B. Pickled you probably haven't heard of them pork belly 8-bit. Church-key roof party palo santo humblebrag street art glossier lumbersexual raw denim mlkshk cred ennui. Thundercats kale chips dreamcatcher celiac la croix portland, brunch fanny pack raw denim 3 wolf moon. Gluten-free pok pok authentic, prism yr occupy YOLO banh mi kombucha drinking vinegar taxidermy. Blog kogi occupy la croix, VHS direct trade butcher chambray.",
-    'teacher': 'Dalai Lhama',
-    'location': 'Big Tent',
-    'picture': Images.flamingYoga,
-    'time': '24:7pm'
+    'name': 'Wanderland - Intro to Sacred Geometry',
+    'bio': "This class will be driven toward those interested in learning more information on Sacred Geometry and how to integrate it's significance in daily life, and to discuss the symbolism of some of the most common designs.",
+    'teacher': 'AJ Vancattenburch',
+    'location': 'Wanderland',
+    'picture': Images.sacredgclass,
+    'time': 'Sat 1:00pm-2:00pm'
   },
   {
-    'name': 'Under Water Basket Weaving',
-    'bio': "Post-ironic pour-over venmo mlkshk drinking vinegar, palo santo PBR&B. Pickled you probably haven't heard of them pork belly 8-bit. Church-key roof party palo santo humblebrag street art glossier lumbersexual raw denim mlkshk cred ennui. Thundercats kale chips dreamcatcher celiac la croix portland, brunch fanny pack raw denim 3 wolf moon. Gluten-free pok pok authentic, prism yr occupy YOLO banh mi kombucha drinking vinegar taxidermy. Blog kogi occupy la croix, VHS direct trade butcher chambray.",
-    'teacher': 'George Lucas',
-    'location': 'Home State',
-    'picture': Images.flamingYoga,
-    'time': '24:7pm'
+    'name': ' Wanderland - Learn to Henna Workshop',
+    'bio': "20 minute presentation on classic mehndi techniques and time to practice and apply henna to yourself or friends",
+    'teacher': 'Riley Enlow',
+    'location': 'Wanderland',
+    'picture': Images.hennaclass,
+    'time': 'Sat 4:00pm - Dark'
   },
   {
-    'name': 'Test',
-    'bio': "Post-ironic pour-over venmo mlkshk drinking vinegar, palo santo PBR&B. Pickled you probably haven't heard of them pork belly 8-bit. Church-key roof party palo santo humblebrag street art glossier lumbersexual raw denim mlkshk cred ennui. Thundercats kale chips dreamcatcher celiac la croix portland, brunch fanny pack raw denim 3 wolf moon. Gluten-free pok pok authentic, prism yr occupy YOLO banh mi kombucha drinking vinegar taxidermy. Blog kogi occupy la croix, VHS direct trade butcher chambray.",
-    'teacher': 'George Lucas',
-    'location': 'Home State',
-    'picture': Images.flamingYoga,
-    'time': '24:7pm'
+    'name': 'Wanderland -  Vaping 101',
+    'bio': " A class designated to inform those interested on the benefits of switching from cigarettes to vaping, as well as a follow-up class for tutorials on vaping tricks by Ling Ling; a professional vape trick artist.",
+    'teacher': 'Dennis (Ling Ling) Kurasachie',
+    'location': 'Wanderland',
+    'picture': Images.vapeclass,
+    'time': 'Sat 4:00pm-5:30pm'
+  }
+  {
+    'name': 'Wanderland -  Smash Bros/Rick and Morty',
+    'bio': "Friendly competition between friends for a Super Smash Bros. tournament in which the winner will win a prize (which is a surprise for now). Also will be hosting a Rick and Morty marathon for those that are already worn out and want to decompress and relax.",
+    'teacher': 'Andrew Zahm',
+    'location': 'Wanderland',
+    'picture': Images.smashbro,
+    'time': 'Sunday'
   }
 ]
