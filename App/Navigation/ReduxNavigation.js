60dd5466292b9651dbb984fb7b7ae39632fb9b69
Sam Kissee <samkissee@gmail.com>
2018-04-27 17:21:22 -0600

    add back button fix to android

--- App/Navigation/ReduxNavigation.js
+++ App/Navigation/ReduxNavigation.js
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, { Component }  from 'react'
 import * as ReactNavigation from 'react-navigation'
 import { connect } from 'react-redux'
 import AppNavigation from './AppNavigation'
 
-// here is our redux-aware smart component
-function ReduxNavigation (props) {
-  const { dispatch, nav } = props
-  const navigation = ReactNavigation.addNavigationHelpers({
-    dispatch,
-    state: nav
-  })
+import { BackHandler } from 'react-native'
+import { addNavigationHelpers, } from 'react-navigation'
 
-  return <AppNavigation navigation={navigation} />
+
+// here is our redux-aware smart component
+class ReduxNavigation extends Component {
+  constructor(props) {
+        super(props);
+    }
+  componentWillMount() {
+      BackHandler.addEventListener('hardwareBackPress', function() {
+          const { dispatch, navigation, nav } = this.props;
+          if (nav.routes.length === 1 && (nav.routes[0].routeName === 'Login' || nav.routes[0].routeName === 'Start')) {
+              return false;
+          }
+          // if (shouldCloseApp(nav)) return false
+          dispatch({ type: 'Navigation/BACK' });
+          return true;
+      }.bind(this));
+  }
+
+  componentWillUnmount() {
+      BackHandler.removeEventListener('hardwareBackPress');
+  }
+
+  render() {
+      return <AppNavigation navigation={addNavigationHelpers({ dispatch: this.props.dispatch, state: this.props.nav })} />
+  }
 }
 
 const mapStateToProps = state => ({ nav: state.nav })
